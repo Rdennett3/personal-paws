747cabd5058063fceab996939d81b0ac6974fe2f
Bob Dennett <r.dennett3@gmail.com>
2021-02-17 17:04:29 -0500

    2-17-21

--- js/index.js
+++ js/index.js
@@ -3,6 +3,26 @@ $('#toggle').click(function() {
   $('#overlay').toggleClass('open');
  });
 
+
+
+// *********************
+// HIDE HEADER ON SCROLL
+// *********************
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("menuContainer").style.top = "0";
+  } else {
+    document.getElementById("menuContainer").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+// *************************
+// END HIDE HEADER ON SCROLL
+// *************************
+
 gsap.registerPlugin(ScrollTrigger);
 
 // const topIntroText = gsap.from('#heroItemAlpha h1',  {
@@ -92,81 +112,203 @@ ScrollTrigger.create({
   onLeaveBack: () => introInfo.pause(0)
 });
 
-// const teaItemOne = gsap.from ('#teamItemOne', {
-//   y:200,
-//   pin:true,
-//   opacity:.5,
-//   duration:1,
-//   ease:'ease-in'
-// });
+const packTitle = gsap.from('#packTitleH1', {
+  y:-25,
+  x:-25,
+  pin:true,
+  opacity:0,
+  duration:1.7,
+  ease:'ease-in-out',
+  paused:true
+});
 
-// const introone = gsap.from ('#gridTopTwo', {
-//   opacity:.7,
-//   y:250,
-//   scale:1.1115,
-//   duration:1,
-//   ease:'ease-in-out',
-//   pin:true,
-// });
+ScrollTrigger.create({
+  trigger:'.packTitle',
+  start:'150px bottom',
+  onEnter: () => packTitle.restart()
+});
 
-// ScrollTrigger.create({
-// trigger:'#gridTopTwo',
-// start: '100px bottom',
-// onEnter: () => introone.restart(0)
-// });
+ScrollTrigger.create({
+  trigger:'.packTitle',
+  start: '-50px bottom',
+  onLeaveBack: () => packTitle.pause(0)
+});
 
-// ScrollTrigger.create({
-//   trigger: '#gridTopTwo',
-//   start:'-200px bottom',
-//   onLeaveBack: () => introone.pause(0),
-// });
+const packOneInfo = gsap.from('#gridTopOne p', {
+  scale:.9,
+  y:25,
+  x:75,
+  pin:true,
+  opacity:0,
+  duration:1.5,
+  ease:'ease-in',
+  paused:true
+});
 
-// const introtwo = gsap.from ('#gridTopOne', {
-//   opacity:.7,
-//   y:200,
-//   autoAlpha:0,
+ScrollTrigger.create({
+  trigger:'#gridTopOne',
+  start:'100px bottom',
+  onEnter: () => packOneInfo.restart()
+});
 
-//   duration:1.5,
-//   ease:'ease-in-out',
-//   pin:true,
-// });
+ScrollTrigger.create({
+  trigger:'#gridTopOne',
+  start: '-50px bottom',
+  onLeaveBack: () => packOneInfo.pause(0)
+});
 
-// const introtwoto = gsap.to ('#gridTopOne', {
-//   opacity:1,
-//   y:0,
-//   autoAlpha:1,
-//   scale:1,
-// });
+const packLeader = gsap.from('#gridTopThree h1', {
+  scale:.9,
+  y:25,
+  x:25,
+  pin:true,
+  opacity:0,
+  duration:1.5,
+  ease:'ease-in',
+  paused:true
+});
 
-// ScrollTrigger.create({
-// trigger:'#gridTopTwo',
-// start: '100px bottom',
-// onEnter: () => introtwo.restart(0)
-// });
+ScrollTrigger.create({
+  trigger:'#gridTopThree',
+  start:'100px bottom',
+  onEnter: () => packLeader.restart()
+});
 
-// ScrollTrigger.create({
-//   trigger: '#gridTopTwo',
-//   start:'-200px bottom',
-//   onLeaveBack: () => introtwo.pause(0),
-// });
+ScrollTrigger.create({
+  trigger:'#gridTopThree',
+  start: '-50px bottom',
+  onLeaveBack: () => packLeader.pause(0)
+});
 
-// const introtwo = gsap.from ('#gridTopOne', {
-//   opacity:.7,
-//   y:-50,
-//   x:-200,
-//   duration:1,
-//   ease:'ease-in-out',
-//   pin:true,
-// });
+const packLeaderImage = gsap.from('#gridTopTwo', {
+  y:25,
+  x:25,
+  pin:true,
+  opacity:0,
+  duration:1.7,
+  ease:'ease-in',
+  paused:true
+});
 
-// ScrollTrigger.create({
-// trigger:'#gridTopOne',
-// start: 'top bottom',
-// onEnter: () => introtwo.restart(0)
-// });
+ScrollTrigger.create({
+  trigger:'#gridTopTwo',
+  start:'100px bottom',
+  onEnter: () => packLeaderImage.restart()
+});
 
-// ScrollTrigger.create({
-//   trigger: '#gridTopOne',
-//   start:'-200px top',
-//   onLeaveBack: () => introtwo.pause(0),
-// });
+ScrollTrigger.create({
+  trigger:'#gridTopTwo',
+  start: '-50px bottom',
+  onLeaveBack: () => packLeaderImage.pause(0)
+});
+
+const gridLowerItemOne = gsap.from('#gridLowerItemOne h1', {
+  y:25,
+  x:-25,
+  pin:true,
+  opacity:0,
+  duration:1.7,
+  ease:'ease-in',
+  paused:true
+});
+
+ScrollTrigger.create({
+  trigger:'#gridLowerItemOne',
+  start:'100px bottom',
+  onEnter: () => gridLowerItemOne.restart()
+});
+
+ScrollTrigger.create({
+  trigger:'#gridLowerItemOne',
+  start: '-50px bottom',
+  onLeaveBack: () => gridLowerItemOne.pause(0)
+});
+
+const gridLowerItemTwo = gsap.from('#gridLowerItemTwo', {
+  y:25,
+  x:-25,
+  pin:true,
+  opacity:0,
+  duration:1.7,
+  ease:'ease-in',
+  paused:true
+});
+
+ScrollTrigger.create({
+  trigger:'#gridLowerItemTwo',
+  start:'100px bottom',
+  onEnter: () => gridLowerItemTwo.restart()
+});
+
+ScrollTrigger.create({
+  trigger:'#gridLowerItemTwo',
+  start: '-50px bottom',
+  onLeaveBack: () => gridLowerItemTwo.pause(0)
+});
+
+const gridLowerItemThree = gsap.from('#gridLowerItemThree p', {
+  y:25,
+  x:-25,
+  pin:true,
+  opacity:0,
+  duration:1.7,
+  ease:'ease-in',
+  paused:true
+});
+
+ScrollTrigger.create({
+  trigger:'#gridLowerItemThree',
+  start:'100px bottom',
+  onEnter: () => gridLowerItemThree.restart()
+});
+
+ScrollTrigger.create({
+  trigger:'#gridLowerItemThree',
+  start: '-50px bottom',
+  onLeaveBack: () => gridLowerItemThree.pause(0)
+});
+
+const testimonialUpper = gsap.from('.testimonialUpper h1', {
+  y:-25,
+  x:-25,
+  pin:true,
+  opacity:0,
+  duration:1.7,
+  ease:'ease-in-out',
+  paused:true
+});
+
+ScrollTrigger.create({
+  trigger:'.testimonialUpper',
+  start:'100px bottom',
+  onEnter: () => testimonialUpper.restart()
+});
+
+ScrollTrigger.create({
+  trigger:'#testimonialUpper',
+  start: '-50px bottom',
+  onLeaveBack: () => testimonialUpper.pause(0)
+});
+
+const testimonialItem = gsap.from('.testimonialItem p', {
+  y:-25,
+  x:-25,
+  pin:true,
+  opacity:0,
+  duration:1.7,
+  ease:'ease-in-out',
+  paused:true
+});
+
+ScrollTrigger.create({
+  trigger:'.testimonialItem p',
+  start:'200px bottom',
+  onEnter: () => testimonialItem.restart()
+});
+
+ScrollTrigger.create({
+  trigger:'.testimonialItem p',
+  start: '-50px bottom',
+  onLeaveBack: () => testimonialItem.pause(0)
+});
+
